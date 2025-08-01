b433d2911e0e8f1c778fd4064ad107ea90d26033
dachi377 <magradzedachi399@gmail.com>
2025-08-01 21:27:33 +0400

    second commit

---
+++ level 004/homework/homework.py
@@ -0,0 +1,17 @@
+dachi="1"
+dachi="2"
+dachi="3"
+dachi="4"
+dachi="5"
+dachi="6"
+dachi="7"
+dachi="8"
+dachi="9"
+dachi="10"
+
+print(snake_case-ი არის როდესაც სიტყვებს შორის ვწერთ _-ს მაგალითად my_name_is)
+print(camelCase-ი არის როდესაც ყოველი სიტყვა იწყება დიდი ასოთი პირველი სიტყვის გარდა)
+print(casesensitivelanguage ნიშნავს რომ დიდი და პატარა ასოები ერთმანეთისგან განსხვავდება)
+print(HTML ვებსაიტის ჩონჩხია)
+print(string=ტექსტი integer=რიცხვი float=წილადი რიცხვი)
+print(HTML-ით წერ CSS-ით აძლევ დიზაინს JS-ამბობ რა ქნას როცა დააწვები)
