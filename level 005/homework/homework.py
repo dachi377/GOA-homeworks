84c7c24e64e65dda3328fe4e4ecbe6733e1c6da7
dachi377 <magradzedachi399@gmail.com>
2025-08-09 14:41:40 +0400

    commit

---
+++ level 005/homework/homework.py
@@ -0,0 +1,11 @@
+#input გამიყენება რომ მომხმარებლისგან მონაცემი მივიღოთ
+#output პროგრამიდან ეკრანზე ინფორმაციის გამოჩენას გამოჩენას
+#ფუნქცია არის კოდის ბლოკი
+name=input(dachi)
+print("hello"+name)
+
+name=input()
+print("hello"+name)
+age=int(input)
+print(age + 10)
+
